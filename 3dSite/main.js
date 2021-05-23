240d8aa1a43a3eabff4ecc0be6681f0e3ed5cf74
Aryan Thatte <aryan.thatte@gmail.com>
2021-05-22 21:57:31 -0400

    torus acc works lol what

--- 3dSite/main.js
+++ 3dSite/main.js
@@ -1,6 +1,7 @@
 import './style.css'
 
 import * as THREE from 'three';
+import { AmbientLight } from 'three';
 
 
 const scene = new THREE.Scene();
@@ -15,25 +16,34 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 camera.position.setZ(30);
-camera.position.setY(30);
-camera.lookAt(0, 0, 0);
+//camera.position.setY(30);
+//camera.lookAt(0, 0, 0);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshBasicMaterial({
-  color: 0xFFFFFF,
-  wireframe: true
-});
-
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347 });
 const torus = new THREE.Mesh(geometry, material);
-
 scene.add(torus);
+
+
+const pointLight = new THREE.PointLight(0xffffff)
+pointLight.position.set(10, 5, 5)
+const ambientLight = new THREE.AmbientLight(0xffffff)
+scene.add(pointLight, ambientLight)
+
+const lightHelper = new THREE.PointLightHelper(pointLight)
+scene.add(lightHelper);
+
 
 function animate() {
   requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
   renderer.render(scene, camera);
 }
-
-animate();
+animate()
 
 
 
